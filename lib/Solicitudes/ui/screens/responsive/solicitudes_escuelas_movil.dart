e887f4fe5cd249516d1c035796388f78ddaf6088
Jesús D Machado <elmachado@hotmail.es>
2022-05-19 11:43:28 -0500

    Add flutter_web Workflow

--- lib/Solicitudes/ui/screens/responsive/solicitudes_escuelas_movil.dart
+++ lib/Solicitudes/ui/screens/responsive/solicitudes_escuelas_movil.dart
@@ -24,47 +24,51 @@ class SolicitudesEscuelasMovil extends StatelessWidget {
 
     FloatingButton floButton = FloatingButton();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Solicitudes de Escuelas'),
-      ),
-      body: StreamBuilder<List<SolicitudEscuelas>>(
-          stream: donanteStream.solicitudEscuelasStream(),
-          builder: (BuildContext context,
-              AsyncSnapshot<List<SolicitudEscuelas>> snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasData) {
-              return Container(
-                padding:
-                    const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: snapshot.data!.toList().length,
-                  itemBuilder: (context, index) {
-                    SolicitudEscuelas currentSolicitud = snapshot.data![index];
-                    return FittedBox(
-                        fit: BoxFit.fill,
-                        child: CardSolicitudesEscuelas(
-                            currentSolicitud.nombreEscuela,
-                            currentSolicitud.nombreResponsable,
-                            currentSolicitud.telefonoResponsable,
-                            currentSolicitud.direccionEscuela,
-                            currentSolicitud.queSolicita));
-                  },
-                ),
-              );
-            } else {
-              return const Center(
-                child: Text('Un error a ocurrido'),
-              );
-            }
-          }),
-      floatingActionButton: floButton.floatingButton(
-          btnAction: () => FormDialogRegistroSolicitudEscuela
-              .formDialogRegistroSolicitudEscuela(context)),
-    );
+        appBar: AppBar(
+          title: const Text('Solicitudes de Escuelas'),
+        ),
+        body: StreamBuilder<List<SolicitudEscuelas>>(
+            stream: donanteStream.solicitudEscuelasStream(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<SolicitudEscuelas>> snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasData) {
+                return Container(
+                  padding:
+                      const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: snapshot.data!.toList().length,
+                    itemBuilder: (context, index) {
+                      SolicitudEscuelas currentSolicitud =
+                          snapshot.data![index];
+                      return FittedBox(
+                          fit: BoxFit.fill,
+                          child: CardSolicitudesEscuelas(
+                              currentSolicitud.nombreEscuela,
+                              currentSolicitud.nombreResponsable,
+                              currentSolicitud.telefonoResponsable,
+                              currentSolicitud.direccionEscuela,
+                              currentSolicitud.queSolicita));
+                    },
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: Text('Un error a ocurrido'),
+                );
+              }
+            }),
+        floatingActionButton: floButton.floatingButton(btnAction: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const FormDialogRegistroSolicitudEscuela();
+              });
+        }));
   }
 }
