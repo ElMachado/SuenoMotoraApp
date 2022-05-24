e8aefc8a5035dcd30434de7743216b8c02b77f9b
Jesus D Machado <elmachado@hotmail.es>
2022-05-24 11:33:02 -0500

    Fix: arregla problemas entre ramas

--- lib/Envios/ui/screens/responsive/envio_desktop.dart
+++ lib/Envios/ui/screens/responsive/envio_desktop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:suenomotora_app/Envios/ui/widgets/card_envio.dart';
 
